1ea6b670104e7105faa09a8c8a98608914ef9054
TsNedev <tsvetannedev360@gmail.com>
2024-09-13 09:04:00 +0300

    adding props to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from"./assets/react-core-concepts.png"
+import componentsImg from "./assets/components.png"
 const reactDescription = ['Fundamental','Crucial','Core']; 
 
 function genRandomInt(max){
@@ -10,7 +12,7 @@ function Header(){
   const description = reactDescription[genRandomInt(2)]
   return(
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
@@ -20,12 +22,30 @@ function Header(){
   );
 }
 
+function CoreConcept(props){
+  return <li>
+  <img src={props.img} alt={props.title}></img>
+  <h3>{props.title}</h3>
+  <p>{props.description}</p>
+  </li>
+}
+
 function App() {
   return (
     <div>
      <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept 
+          title ="Components" 
+          description="The core UI building block." 
+          img = {componentsImg}
+          />
+        </ul>
+        </section>
+       
       </main>
     </div>
   );
