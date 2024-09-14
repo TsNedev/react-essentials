ee77727b335471e96316473677b75329ae2a3b4e
TsNedev <tsvetannedev360@gmail.com>
2024-09-14 15:40:40 +0300

    Components Split
    Getting the components in separate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import reactImg from"./assets/react-core-concepts.png"
-import componentsImg from "./assets/components.png"
-const reactDescription = ['Fundamental','Crucial','Core']; 
-
-function genRandomInt(max){
-  return Math.floor(Math.random()*(max+1));
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import {CORE_CONCEPTS} from "./data.js"
 
 
-function Header(){
 
-  const description = reactDescription[genRandomInt(2)]
-  return(
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header> 
-  );
-}
 
-function CoreConcept(props){
-  return <li>
-  <img src={props.img} alt={props.title}></img>
-  <h3>{props.title}</h3>
-  <p>{props.description}</p>
-  </li>
-}
+
 
 function App() {
   return (
@@ -38,11 +15,10 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcept 
-          title ="Components" 
-          description="The core UI building block." 
-          img = {componentsImg}
-          />
+          <CoreConcept {...CORE_CONCEPTS[0]}/>
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
         </ul>
         </section>
        
