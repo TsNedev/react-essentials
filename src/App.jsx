442b5dcda67bafdcc26d192e9538b59a8b4cade2
TsNedev <tsvetannedev360@gmail.com>
2024-09-16 13:30:43 +0300

    adding useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
-import {CORE_CONCEPTS} from "./data.js"
-
+import { CORE_CONCEPTS } from "./data.js"
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 
 
 
 
 function App() {
+const[selectedTopic, setSelectedTopic]=useState('please click a button'); 
+  let tabContent ='please click a  button';
+
+  function handleClick(selectedButton){
+    setSelectedTopic(selectedButton)
+    
+        }
+
   return (
+    
     <div>
-     <Header></Header>
+      <Header></Header>
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]}/>
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
-       
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=>handleClick('components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleClick('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleClick('props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleClick('state')}>State</TabButton>
+          </menu>
+            {selectedTopic}
+        </section>
       </main>
     </div>
   );
